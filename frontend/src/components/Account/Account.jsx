ab1665ffb6f9a42fcd16db37828da9dc021b13d7
Alevtina Brown <alevtinabrown@Alevtinas-MacBook-Pro.local>
2021-09-02 15:07:54 +0300

    added styles in account

--- frontend/src/components/Account/Account.jsx
+++ frontend/src/components/Account/Account.jsx
@@ -3,25 +3,23 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { GET_ALL_MARKERS_DB } from "../../urls/url";
 import Mark from "../Mark/Mark";
-import "./Account.css";
+import stylesAcc from "./Account.module.css";
 
 const Account = () => {
   // const arr = [{ coords: [55.72242171789997, 37.55932930905152], adress: 'Россия, Москва, улица Хамовнический Вал, 36' }, { coords: [55.76069738614288, 37.64234904248048], adress: 'Россия, Москва, Чистопрудный бульвар, 12к7А' }];
-  const [markers, setMarkers] = useState([])
-  const [markersValue, setMarkersValue] = useState(0)
-  const [flag, setFlag] = useState(null)
-  const [allMarkers, setAllMarkers] = useState([])
+  const [markers, setMarkers] = useState([]);
+  const [markersValue, setMarkersValue] = useState(0);
+  const [flag, setFlag] = useState(null);
+  const [allMarkers, setAllMarkers] = useState([]);
 
+  const allMarks = useSelector((state) => state.marks);
 
-  const allMarks = useSelector(state => state.marks)
+  const allAcceptedMarks = allMarks.filter((el) => el.isAccepted === true);
 
-  const allAcceptedMarks = allMarks.filter(el => el.isAccepted === true)
+  const allNewMarks = allMarks.filter((el) => el.isChecked === false);
 
-  const allNewMarks = allMarks.filter(el => el.isChecked === false)
-
-  console.log('NEWMARKS', allNewMarks)
+  console.log("NEWMARKS", allNewMarks);
   // const newMarks = useSelector(state => state.newMarks)
-
 
   // useEffect(() => {
   // axios.get(GET_ALL_MARKERS_DB).then(res => { // gets new markers
@@ -35,39 +33,65 @@ const Account = () => {
   //   })
   // }, [])
 
-
   const newMarkersHandler = () => {
-    setFlag(true)
-  }
+    setFlag(true);
+  };
 
   const allMarkersHandler = () => {
-    setFlag(false)
-  }
+    setFlag(false);
+  };
 
-  // console.log(arr)
   return (
-    <>
-      <div className="block-wrapper__map">
-        <div className='map'>
-          <div className='centring'>
-            <h3> Выберите </h3>
-            <button className='btn btn-success mx-2' onClick={newMarkersHandler}> Новые метки </button>
-            <button className='btn btn-success' onClick={allMarkersHandler}> Вывести все метки </button>
-          </div>
-          {flag ? allNewMarks.map(el => <Mark id={el.id} identificator={true} longitude={el.longitude}
-            latitude={el.latitude} adress={el.address} pic={el.pics} key={el.id} />)
-            :
-            allAcceptedMarks.map(el => <Mark id={el.id} identificator={false} longitude={el.longitude}
-              latitude={el.latitude} adress={el.address} pic={el.pics} key={el.id} />)}
-        </div>
-        <div className='shit' id='infoBlock'>
-          <h2>Добро пожаловать в ЛК</h2>
-          <p>Admin</p>
-          <p>Всего заметок на сайте: {markersValue}</p>
+    <div className={stylesAcc.containerAcc}>
+      <div className={stylesAcc.wrapper}>
+        {/* <div className="block-wrapper__map"> */}
+        {/* <div className="map"> */}
+        {/* <div className="centring"> */}
+
+        {/* <div className="shit" id="infoBlock"> */}
+        <div className={stylesAcc.lk_card}>
+          <h2>Личный кабинет администратора</h2>
+          <p>Всего меток на сайте: {markersValue}</p>
         </div>
 
+        <h3> Выберите действие </h3>
+        <button className={stylesAcc.button_marks} onClick={newMarkersHandler}>
+          {" "}
+          Новые метки{" "}
+        </button>
+        <button className={stylesAcc.button_marks} onClick={allMarkersHandler}>
+          {" "}
+          Вывести все метки{" "}
+        </button>
+        {/* </div> */}
+
+        {flag
+          ? allNewMarks.map((el) => (
+              <Mark
+                id={el.id}
+                identificator={true}
+                longitude={el.longitude}
+                latitude={el.latitude}
+                adress={el.address}
+                pic={el.pics}
+                key={el.id}
+              />
+            ))
+          : allAcceptedMarks.map((el) => (
+              <Mark
+                id={el.id}
+                identificator={false}
+                longitude={el.longitude}
+                latitude={el.latitude}
+                adress={el.address}
+                pic={el.pics}
+                key={el.id}
+              />
+            ))}
       </div>
-    </>
+    </div>
+    // </div>
+    // </div>
   );
 };
 
--- frontend/src/components/SendingForm/SendingForm.jsx
+++ frontend/src/components/SendingForm/SendingForm.jsx
@@ -1,42 +1,49 @@
-import './SendingForm.css'
+import "./SendingForm.css";
 import React, { useState } from "react";
 import { useDropzone } from "react-dropzone";
 
+const SendingForm = ({
+  sendForm,
+  handleImageUpload,
+  imageUploader,
+  uploadedImage,
+  setFiles,
+  files,
+}) => {
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: "image/*",
+    onDrop: (acceptedFiles) => {
+      setFiles((prevFiles) => [
+        ...prevFiles,
+        ...acceptedFiles.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        ),
+      ]);
+    },
+  });
 
-const SendingForm = ({ sendForm, handleImageUpload, imageUploader, uploadedImage, setFiles, files }) => {
-    const { getRootProps, getInputProps } = useDropzone({
-      accept: "image/*",
-      onDrop: (acceptedFiles) => {
-        setFiles((prevFiles) => [
-          ...prevFiles,
-          ...acceptedFiles.map((file) =>
-            Object.assign(file, {
-              preview: URL.createObjectURL(file),
-            })
-          ),
-        ]);
-      },
-    });
-
-    const images = files.map((file) => (
-      <div key={file.name}>
-        <div>
-          <img src={file.preview} style={{ width: "200px" }} alt="preview" />
-        </div>
+  const images = files.map((file) => (
+    <div key={file.name}>
+      <div>
+        <img src={file.preview} style={{ width: "100%" }} alt="preview" />
       </div>
-    ));
+    </div>
+  ));
 
   return (
-
     <form onSubmit={sendForm} className="form">
-      <h5>Добавить метку</h5>
-      <p type="Добавьте комментарий">
-        <input type="text"
-          placholder='comment'
+      <legend className="text-center text-light">Добавьте метку</legend>
+      <div className="mb-3">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="comment"
           name="comment"
         />
-      </p>
-{/* 
+      </div>
+      {/* 
       <p type="Вставьте изображение">
         <input
           type="file"
@@ -47,29 +54,24 @@ const SendingForm = ({ sendForm, handleImageUpload, imageUploader, uploadedImage
         />
       </p> */}
 
-      <div>
-        <div className='formAddImage' {...getRootProps()}>
+      <div className="mb-3 draganddrop">
+        <div className='dragPic' {...getRootProps()}>
           <img
-            className='add'
+            className="add"
             src="'images/—Pngtree—plus vector icon_4236965.png"
             alt=""
           />
           <input {...getInputProps()} />
-          <p>Add minimum 1 photo</p>
         </div>
-        <div className='addImages'>{images}</div>
       </div>
+        <div className="addImages">{images}</div>
 
-      <img className='inpimg'
-        ref={uploadedImage}
-      />
+      <img className="inpimg" ref={uploadedImage} />
 
-
-      <button>Send Message</button>
+      <button className="button_send">Send Message</button>
       {/* </div> */}
     </form>
+  );
+};
 
-  )
-}
-
-export default SendingForm
+export default SendingForm;
